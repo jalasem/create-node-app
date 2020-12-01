ac3aaa320583db523424267bef2ee066ffbeacb9
Abdulsamii Ajala <kgasta@gmail.com>
2020-12-01 16:19:07 +0100

    feat: skip passed args in question prompts

--- index.js
+++ index.js
@@ -1,19 +1,4 @@
 #!/usr/bin/env node
-const inquirer = require("inquirer");
+const { runTasks } = require("./lib/tasks");
 
-const { configs, questions } = require("./lib/questions");
-
-(async () => {
-  try {
-    const responses = await inquirer.prompt(questions);
-
-    const configuration = {
-      ...configs,
-      ...responses,
-    };
-
-    console.log(JSON.stringify(configuration, null, 2));
-  } catch (err) {
-    console.log(err);
-  }
-})();
+runTasks();
